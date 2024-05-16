189ecfb02fbeddafa8a1b306b2497f6a97ee16cc
d3ciee <decefemz@gmail.com>
2024-05-16 02:49:10 +0200

    Add UserSession relations and rename User types export to TUser

--- src/lib/models/user/user-session.ts
+++ src/lib/models/user/user-session.ts
@@ -1,5 +1,6 @@
 import User from "./user";
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core"
+import {relations} from "drizzle-orm"
 
 const UserSession = sqliteTable("user_session", {
     id: text("id").notNull().primaryKey(),
@@ -9,5 +10,14 @@ const UserSession = sqliteTable("user_session", {
     expiresAt: integer("expires_at").notNull()
 });
 
+const userSessionRelations = relations(UserSession,({one})=>({
+    user: one(User, {
+        fields: [UserSession.userId],
+        references: [User.id]
+        
+    })
+}))
+
 export type TUserSession = typeof UserSession.$inferSelect;
+export {userSessionRelations}
 export default UserSession;
--- src/lib/models/user/user.ts
+++ src/lib/models/user/user.ts
@@ -4,5 +4,7 @@ const User = sqliteTable("user", {
     id: text("id").notNull().primaryKey(),
 })
 
-export type User = typeof User.$inferSelect;
+export type TUser = typeof User.$inferSelect;
 export default User;
+
+
